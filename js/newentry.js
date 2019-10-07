cafd2c508046deea34e8bc6dddff6c898d3f4a0a
Aphrodis <izabayoaphrodis@gmail.com>
2019-10-07 16:43:31 +0200

    Initial commit for UI templates

--- js/newentry.js
+++
@@ -1,8 +0,0 @@
-const newEntry = document.querySelector('.js-form-newEntry');
-
-const submitEntry = (e) => {
-	e.preventDefault();
-	setTimeout(() => {window.location.href = '../html/allentries.html';}, 1000);
-};
-
-if (newEntry) newEntry.addEventListener('submit', submitEntry);
--- js/signin.js
+++
@@ -1,8 +0,0 @@
-const signinForm = document.querySelector('.signin-form');
-
-const processSubmit = (e) => {
-	e.preventDefault();
-	setTimeout(() => {window.location.href = '../html/dashboard.html';}, 1000);
-};
-
-if (signinForm) signinForm.addEventListener('submit', processSubmit);
